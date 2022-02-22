f766c31b87b8e717b03db2282d6447baa0c5a9d8
Peter Newstein <peternewstein@gmail.com>
2022-02-22 12:00:52 -0800

    fixed cli

--- layer2svg/__main__.py
+++ layer2svg/__main__.py
@@ -1,22 +1,36 @@
 import sys
 from pathlib import Path
+import argparse
 
 from layer2svg import layer2svg, Mode
 
 def main():
-    if len(sys.argv) == 1:
-        raise(ValueError("no args"))
-    if len(sys.argv) >= 3:
-        mode_str = sys.argv[-1]
-        if mode_str == "layer":
-            mode = Mode.layer
-        elif mode_str == "build":
-            mode = Mode.build
-        elif mode_str == "number":
-            mode = Mode.number
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="The svg file to process")
+    parser.add_argument("-o", "--out-directory", help="The directory where the output svgs are put. "
+                        "Defaults to a folder with the same name as the input file",
+                        default=None)
+    mode_arg = parser.add_mutually_exclusive_group()
+    mode_arg.add_argument("-l", "--layer", 
+                          help="Use layer mode: each layer not containing the word background is stored as a separate svg",
+                          action="store_true")
+    mode_arg.add_argument("-b", "--build", 
+                          help="Use build mode: layers are build on top of each other",
+                          action="store_true")
+    mode_arg.add_argument("-n", "--number", 
+                          help="Use number mode: layers containing a number are combined into a svg for each number",
+                          action="store_true")
+    args = parser.parse_args()
+    if args.build == True:
+        mode = Mode.build
+    elif args.number == True:
+        mode = Mode.number
     else:
         mode = Mode.layer
-    infile = sys.argv[1]
-    layer2svg(Path(infile), mode=mode)
+    if args.out_directory is not None:
+        export_folder = Path(args.out_directory)
+    else:
+        export_folder = None
+    layer2svg(Path(args.file), export_folder=export_folder, mode=mode)
 
 main()
